743f1d46b0499bfd1dfd23d61028d8119936faba
KirshWasHere <brrranuel006@gmail.com>
2025-07-05 12:29:06 -0700

    Hi :)

---
+++ launch_roblox.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+def launch_roblox(uri: str):
+    if not uri:
+        return
+
+    try:
+        os.startfile(uri)
+    except Exception:
+        pass
+
+if __name__ == "__main__":
+    example_uri = "roblox://placeId=606849621" 
+
+    if len(sys.argv) > 1:
+        example_uri = sys.argv[1]
+    
+    launch_roblox(example_uri)
